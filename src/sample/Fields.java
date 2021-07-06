f8392b50b128ecd56c501014cbba1bbcce995df5
Boris P <32775983+bpisabwolf@users.noreply.github.com>
2021-07-06 13:40:07 -0500

    Added Labels and textfields to start forming form Made using GridPane layout. Will play with it more.

--- src/sample/Fields.java
+++ src/sample/Fields.java
@@ -1,4 +1,68 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+
+
+//application idea so far
+//Design
+    /*
+    Date: __________ - Data entered: String date
+
+    TITLE TODAY ----------------------------------------------
+    Tasks left: __________ - Must be split into two catagories,
+        forward pallet      - Integer
+        p pallet            - Integer
+        total pallet        - Integer
+    Inbound Left to unload _______ - Integer
+    Outbounds left to load _______ - Intefer
+    Regular labor hours left _______ - Double
+    OT Labor hours left ________ - Double
+        NOTE: This could potentially have many people listed.
+        Suggested working idea:
+        How many regular labor hours do you want to report? Enter integer of workers
+            New prompt appears with Name (String) and hours (Double)
+        Same for OT Labor hours
+
+
+        //How juan explains it.
+        There is a schedule for people for everyday
+        The idea is that the form sends date information to a database,
+        where it can retrieve who is working there.
+        From there, it will autofill fields for the name of employees
+        whoever is filling the form can manually put hours left after each employee
+        (maybe have employees be selected from a drop down if can't autofill)
+    TITLE TOMORROW --------------------------------------------
+    Inbounds scheduled __________ - Integer
+    FLOOR loaded inbounds scheduled __________ - Integer
+    Railcars ______________ - Integer
+    Full Pallet picks _____________ - Integer
+    Partial Pallet Picks ____________ - Integer
+    Palletes on Floor ___________ - Integer
+     */
+
 public class Fields {
+
+    private ArrayList<Label> labels = new ArrayList<>();
+    private ArrayList<TextField> dataEntry = new ArrayList<>();
+    public Fields(){
+
+    }
+
+    private void createLabels(){
+        Label Date = new Label("Today's Date");
+        labels.add(Date);
+        Label Tasks = new Label("Tasks Left");
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,45 +4,23 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.*;
+import org.w3c.dom.Text;
 
 public class Main extends Application {
 
-    //application idea so far
-    //Design
-    /*
-    Date: __________ - Data entered: String date
 
-    TITLE TODAY ----------------------------------------------
-    Tasks left: __________ - Must be split into two catagories,
-        forward pallet      - Integer
-        p pallet            - Integer
-        total pallet        - Integer
-    Inbound Left to unload _______ - Integer
-    Outbounds left to load _______ - Intefer
-    Regular labor hours left _______ - Double
-    OT Labor hours left ________ - Double
-        NOTE: This could potentially have many people listed.
-        Suggested working idea:
-        How many regular labor hours do you want to report? Enter integer of workers
-            New prompt appears with Name (String) and hours (Double)
-        Same for OT Labor hours
 
-    TITLE TOMORROW --------------------------------------------
-    Inbounds scheduled __________ - Integer
-    FLOOR loaded inbounds scheduled __________ - Integer
-    Railcars ______________ - Integer
-    Full Pallet picks _____________ - Integer
-    Partial Pallet Picks ____________ - Integer
-    Palletes on Floor ___________ - Integer
-     */
-    @Override
     public String textToSave = "Nothing";
+    public int fp, pp, totalP;
+    @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
@@ -54,7 +32,59 @@ public class Main extends Application {
         TextField b = new TextField("First Name");
         TextField c = new TextField("Last Name");
 
-        TilePane tp = new TilePane();
+
+        //Date Label and TextField
+        Label cd = new Label("Today's Date");
+        TextField dateEntry = new TextField("dd/mm/yyyy");
+        //Tasks
+        Label fptl = new Label("FP Tasks Left: ");
+        TextField fpEntry = new TextField("# of FP tasks");
+        Label pptil = new Label("PP Tasks Left");
+        TextField ppEntry = new TextField("# of PP Tasks");
+        //Total tasks will be calculated later
+        Label totalTasksReported;
+
+        //Inbount and outbound entries
+        Label ib = new Label("Inbound Left to Unload: ");
+        TextField inboundEntry = new TextField("# to unload");
+        Label ob = new Label("Outbound Left o Load: ");
+        TextField outboundEntry = new TextField("# to load");
+
+        Label hl = new Label("Regular and OT Hours Left");
+        TextField employeeHourEntry =  new TextField("# of people");
+        //will be drop down where people can be selected from server
+        //must set up database for this
+
+        //FIELDS FOR TOMORROW SECTION
+        Label inSh = new Label("Inbounds Scheduled: ");
+        TextField inSchedEntry = new TextField("# of scheduled");
+
+        Label flInSh =  new Label("FLOOR loaded inbounds scheudled");
+        TextField floorInEntry =  new TextField("# of scheduled");
+
+        Label rc = new Label("Railcars");
+        TextField railcarEntry = new TextField("# of cars");
+
+        Label plf = new Label("Pallets on Floor: ");
+        TextField palletEntry = new TextField("# on Floor");
+
+        //Gonna try GridPane
+        GridPane g = new GridPane();
+        //TilePane tp = new TilePane();
+        g.setPadding(new Insets(10, 10, 10, 10));
+        g.setMinSize(300, 300);
+        g.setVgap(5);
+        g.setHgap(5);
+
+        g.add(cd, 0, 0);
+        g.add(dateEntry, 1, 0);
+
+
+        //getChildren works mostly with stack pains
+        //g.getChildren().add(cd);
+        //g.getChildren().add(dateEntry);
+        /*
+
         Label l = new Label("no text");
         Label fn = new Label("Enter First Name:");
         Label ln = new Label("Enter Last Name: ");
@@ -81,9 +111,10 @@ public class Main extends Application {
         tp.getChildren().add(l);
         // add textfield
         //r.getChildren().add(b);
-
-        Scene sc = new Scene(tp,200,200);
+           */
+        //Scene sc = new Scene(tp,600,600);
         //primaryStage.setScene(new Scene(root, 300, 275));
+        Scene sc = new Scene(g,300,600);
         primaryStage.setScene(sc);
 
 
